9699666cc92cad5e84575b2fdbf1c6cc66efb526
Zak Zubair <zakaaf@hotmail.co.uk>
2020-04-09 03:18:55 +0100

    Minor test update

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -7,3 +7,7 @@ test('get time elapsed in minutes for 30 sec', () => {
 test('get time elapsed in minutes for 120 sec', () => {
   expect(getTimeElapsedInMinutes(120)).toBe('2:00');
 });
+
+test('get time elapsed in minutes for 150 sec', () => {
+  expect(getTimeElapsedInMinutes(150)).toBe('2:30');
+});
